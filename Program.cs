3f804b4a26d5ce68b18ce44b87914a312880bb7b
l_darebnicek <l_darebnicek@utb.cz>
2025-02-12 15:17:30 +0100

    (d)oddelany komenty

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
-///█ ■
-////https://www.youtube.com/watch?v=SGZgvMwjq2U
+
+//komenty done
+
 namespace Snake
 {
     class Program
@@ -151,4 +152,3 @@ namespace Snake
         }
     }
 }
-//¦
